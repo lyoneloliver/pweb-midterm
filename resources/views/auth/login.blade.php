8935b32cc4f909298b2bb017e3de971b52449b2b
Pascalllllll <134050834+Pascalllllll@users.noreply.github.com>
2025-10-22 12:17:39 +0700

    css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
     {{-- Link to your custom CSS --}}
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
     
-    {{-- Google Fonts: Inter --}}
+    {{-- Google Fonts: Inter (for modern, clean look) --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
